03c59debf7ef9e2e4303b332df147319c4568d50
sunny1953 <142408101+sunny1953@users.noreply.github.com>
2025-11-26 13:13:49 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,4 @@
 docker stop simple-python-container || true
 docker rm simple-python-container || true
 
-docker run -d \
-  --name simple-python-container \
-  -p 5000:5000 \
-  sunny1953/simple-python-app
+docker run -d -p 5000:5000 sunny1953/simple-python-app
